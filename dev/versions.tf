b055d0627b70ecba3ec1113ad54a17261ce370cf
Joonas Venäläinen <joonvena@gmail.com>
2024-06-27 16:22:27 +0300

    Include lockfile for dev laos

--- dev/versions.tf
+++ dev/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.35.0"
+      version = "5.30.0"
     }
     random = {
       source = "hashicorp/random"
-      version = "3.6.2"
+      version = "3.6.0"
     }
   }
 }
