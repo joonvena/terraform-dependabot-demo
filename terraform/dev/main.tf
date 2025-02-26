23e3575ac76ca9100b7caa328c7589b71cdeef8e
Joonas Venäläinen <joonvena@gmail.com>
2025-02-26 14:12:42 +0200

    test

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -3,7 +3,3 @@ resource "random_string" "random" {
   special          = true
   override_special = "/@£$"
 }
-
-module "aiven" {
-  source = "git@github.com:joonvena/terraform-aiven-posgres.git?ref=main"
-}
